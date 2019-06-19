5318896e9ac19f1ff3dc532d0b524a754a714b9a
lileowang <wangli20@hotmail.com>
2019-06-19 16:25:27 -0700

    Created a new Python project in Visual Studio 2017

---
+++ Project/Demo/Demo/Demo.py
@@ -0,0 +1,17 @@
+# Author:      Li Leo Wang
+# Start Date:  2019-06-19
+# Description:
+#      - A test harness for temporary trying out code.
+# Notes:
+#      - (none)
+#
+# Change history:
+#      - Refer to GitHub comments related to each source file.
+#
+
+import sys
+def main():
+    input('\nEnd')
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
