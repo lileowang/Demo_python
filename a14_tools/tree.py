e11f8cd9d6f314178ee83dd8c827d9c04dfb71db
lileowang <wangli20@hotmail.com>
2020-08-22 22:07:52 -0700

    use more descriptive name for the marker: middle

--- a14_tools/tree.py
+++ a14_tools/tree.py
@@ -12,10 +12,11 @@ https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-pyt
 import sys
 from pathlib import Path
 from itertools import islice
+# import pdb
 
 space = '  '
 branch = '|  '
-tee = '|--'
+mid = '|--'
 last = '|__'
 
 
@@ -38,6 +39,9 @@ def tree(folder: Path,
     def inner(folder: Path, prefix: str = '', depth: int = -1):
         nonlocal files, dirs
 
+        # debug only
+        # pdb.set_trace()
+
         if not depth:
             return  # 0, reach required tree depth
 
@@ -51,7 +55,7 @@ def tree(folder: Path,
         # print(contents)
 
         # collections of tree node markers
-        markers = [tee] * (len(contents) - 1) + [last]
+        markers = [mid] * (len(contents) - 1) + [last]
         # print(markers)
 
         # loop thru each node
@@ -60,7 +64,7 @@ def tree(folder: Path,
                 # enclose folder name with square brackets []
                 yield prefix + marker + '[' + path.name + ']'
                 dirs += 1
-                extension = branch if marker == tee else space
+                extension = branch if marker == mid else space
                 yield from inner(path,
                                  prefix=prefix + extension,
                                  depth=depth - 1)
@@ -75,21 +79,22 @@ def tree(folder: Path,
     for line in islice(iterator, max_len):
         print(line)
     if next(iterator, None):
+        # iterator is not None, not exhausted
         print(f'... max_len: {max_len}, reached')
     print(f'\n{dirs} subfolders, {files} files')
 
 
 if __name__ == '__main__':
-    # print(f'{Path.cwd()}:')
     if len(sys.argv) < 4:
         print('Args: [dir_only] [depth] [max_len]')
         print('example: 0 -1 -1')
         print('dir_only: 0 = files and subfolder, 1 = subfolder only')
         print('depth: -1 = full depth, positive number n = n levels')
-        print('max_len: -1 = default (1_000 items), positive number n = max n items')
+        print(
+            'max_len: -1 = default (1_000 items), positive number n = max n items'
+        )
     else:
         dir_only = False if int(sys.argv[1]) == 0 else True
         depth = int(sys.argv[2])
         max_len = 1_000 if int(sys.argv[3]) < 0 else int(sys.argv[3])
         tree(Path.cwd(), dir_only, depth, max_len)
-
